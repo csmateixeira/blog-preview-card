919fe85f78912d37d6337b22b1c1d0f8a7ff4525
Carla T <csmateixeira@users.noreply.github.com>
2024-12-11 16:53:11 +0000

    Added proper shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ["./index.html"],
   theme: {
     extend: {
+      colors: {
+        yellow: "#F4D04E",
+        'gray-950': '#111111',
+        'gray-500': '#6B6B6B'
+      },
+      boxShadow: {
+        thick: "10px 10px 0px black",
+      },
     }
   },
   plugins: [],
